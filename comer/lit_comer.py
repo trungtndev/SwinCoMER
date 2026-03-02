21ce48205d22d6f374e58e505476cc679e2cd9f8
Trung <trunglh113@gmail.com>
2026-03-02 14:45:15 +0700

    init

--- comer/lit_comer.py
+++ comer/lit_comer.py
@@ -120,7 +120,7 @@ class LitCoMER(pl.LightningModule):
             self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, sync_dist=True)
             return loss
 
-    @torch.no_grad()
+    @torch.inference_mode()
     def validation_step(self, batch: Batch, _):
         tgt, out = to_bi_tgt_out(batch.indices, self.device)
         output = self(batch.imgs, batch.mask, tgt)
