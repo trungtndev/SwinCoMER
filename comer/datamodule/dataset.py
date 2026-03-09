9031e4350899e06ea88a302fe28ba3e25da3d56a
Trung <trunglh113@gmail.com>
2026-03-09 07:51:04 +0700

    init

--- comer/datamodule/dataset.py
+++ comer/datamodule/dataset.py
@@ -6,7 +6,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
-from .transforms import ScaleAugmentation, ScaleToLimitRange
+from .transforms import AlbScaleAugmentation, AlbScaleToLimitRange, ScaleToLimitRange, ScaleAugmentation
 
 K_MIN = 0.7
 K_MAX = 1.4
@@ -14,7 +14,46 @@ K_MAX = 1.4
 H_LO = 16
 H_HI = 256
 W_LO = 16
-W_HI = 512
+W_HI = 1024
+
+# class CROHMEDataset(Dataset):
+#     def __init__(self, ds, is_train: bool, scale_aug: bool) -> None:
+#         super().__init__()
+#         self.ds = ds
+#
+#         trans_list = []
+#         if is_train and scale_aug:
+#             trans_list.append(AlbScaleAugmentation(K_MIN, K_MAX))
+#
+#         trans_list += [
+#             AlbScaleToLimitRange(w_lo=16, w_hi=512, h_lo=16, h_hi=256),
+#             A.PadIfNeeded(
+#                 min_height=256,
+#                 min_width=512,
+#                 fill=0,
+#                 position="center"
+#             ),
+#             A.ToRGB(),
+#             A.Normalize(
+#                 mean=(0.485, 0.456, 0.406),
+#                 std=(0.229, 0.224, 0.225)
+#             ),
+#             ToTensorV2(),
+#
+#         ]
+#         self.transform = A.Compose(trans_list)
+#
+#     def __getitem__(self, idx):
+#         fname, img, caption = self.ds[idx]
+#
+#         # img = [self.transform(im) for im in img]
+#         img = self.transform(image=np.array(img))["image"]
+#
+#         return fname, img, caption
+#
+#     def __len__(self):
+#         return len(self.ds)
+
 
 class CROHMEDataset(Dataset):
     def __init__(self, ds, is_train: bool, scale_aug: bool) -> None:
@@ -27,33 +66,20 @@ class CROHMEDataset(Dataset):
 
         trans_list += [
             ScaleToLimitRange(w_lo=W_LO, w_hi=W_HI, h_lo=H_LO, h_hi=H_HI),
-            A.PadIfNeeded(
-                min_height=256,
-                min_width=512,
-                fill=0,
-                position="center"
-            ),
-            A.ToRGB(),
-            A.Normalize(
-                mean=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225)
-            ),
-            ToTensorV2(),
-
+            tr.ToTensor(),
         ]
-        self.transform = A.Compose(trans_list)
+        self.transform = tr.Compose(trans_list)
 
     def __getitem__(self, idx):
         fname, img, caption = self.ds[idx]
 
         # img = [self.transform(im) for im in img]
-        img = self.transform(image=np.array(img))["image"]
+        img = self.transform(np.array(img))
 
         return fname, img, caption
 
     def __len__(self):
         return len(self.ds)
-
 
 # class CROHMEDataset(Dataset):
 #     def __init__(self, ds, is_train: bool, scale_aug: bool) -> None:
