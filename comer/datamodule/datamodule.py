6855f0d52776a0e10552295f2760713b70bb7e48
Trung <trunglh113@gmail.com>
2026-03-09 14:32:26 +0700

    init

--- comer/datamodule/datamodule.py
+++ comer/datamodule/datamodule.py
@@ -121,12 +121,6 @@ class Batch:
 
 
 def collate_fn(batch):
-    # assert len(batch) == 1
-    # batch = batch[0]
-    # fnames = batch[0]
-    # images_x = batch[1]
-    # seqs_y = [vocab.words2indices(x) for x in batch[2]]
-
     fnames = [b[0] for b in batch]
     images_x = [b[1] for b in batch]
     seqs_y = [vocab.words2indices(b[2]) for b in batch]
@@ -137,8 +131,9 @@ def collate_fn(batch):
     n_samples = len(heights_x)
     max_height_x = max(heights_x)
     max_width_x = max(widths_x)
+    n_chanel_x = images_x[0].size(0)
 
-    x = torch.zeros(n_samples, 1, max_height_x, max_width_x)
+    x = torch.zeros(n_samples, n_chanel_x, max_height_x, max_width_x)
     x_mask = torch.ones(n_samples, max_height_x, max_width_x, dtype=torch.bool)
     for idx, s_x in enumerate(images_x):
         x[idx, :, : heights_x[idx], : widths_x[idx]] = s_x
