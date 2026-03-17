2eec50c84cd8aaba4e90456bae75ae1a8568fc56
Trung <trunglh113@gmail.com>
2026-03-17 10:09:52 +0700

    init

--- comer/model/decoder.py
+++ comer/model/decoder.py
@@ -60,30 +60,24 @@ class FFN(nn.Module):
 class MoE(nn.Module):
     def __init__(self, d_model, dim_feedforward, dropout, num_experts):
         super(MoE, self).__init__()
-        # if dist.is_initialized():
-        #     world_size = dist.get_world_size()
-        # else:
-        #     world_size = 1
-        # assert num_experts % world_size == 0, \
-        #     "num_experts must be divisible by world_size"
-        # num_local_experts = num_experts // world_size
-        #
-        # warnings.warn(f"MoE world size: {world_size}")
-        # if num_local_experts == num_experts:
-        #     warnings.warn(f"Using MoE with {num_experts} experts on a single device.")
+        if dist.is_initialized():
+            world_size = dist.get_world_size()
+        else:
+            world_size = 1
+        assert num_experts % world_size == 0, \
+            "num_experts must be divisible by world_size"
+        num_local_experts = num_experts // world_size
 
-        # self.moe = BaseMOELayer(
-        #     BaseTop2Gate(model_dim=d_model, num_experts=num_experts),
-        #     nn.ModuleList([
-        #         FFN(d_model, dim_feedforward, dropout)
-        #         for _ in range(num_local_experts)
-        #     ])
-        # )
-        self.moe = DSMOELayer(
-            hidden_size=d_model,
-            expert=FFN(d_model, dim_feedforward, dropout),
-            num_experts=num_experts,
-            k=2,
+        warnings.warn(f"MoE world size: {world_size}")
+        if num_local_experts == num_experts:
+            warnings.warn(f"Using MoE with {num_experts} experts on a single device.")
+
+        self.moe = BaseMOELayer(
+            BaseTop2Gate(model_dim=d_model, num_experts=num_experts),
+            nn.ModuleList([
+                FFN(d_model, dim_feedforward, dropout)
+                for _ in range(num_local_experts)
+            ])
         )
 
     def forward(self, x):
@@ -91,10 +85,11 @@ class MoE(nn.Module):
         # l_seq, b_size, d_model = x.shape
         # x = rearrange(x, "l b d -> (b l) 1 d")
         x = rearrange(x, "l b d -> b l d")
-        x, l_aux, exp_counts = self.moe(x)
+        x = self.moe(x)
         x = rearrange(x, "b l d -> l b d")
         # x = rearrange(x, "(b l) 1 d -> l b d", b=b_size, l=l_seq)
-        return x, l_aux
+        return x, self.moe.l_aux
+
 
 
 class TransformerDecoderLayer(nn.Module):
@@ -116,57 +111,6 @@ class TransformerDecoderLayer(nn.Module):
         else:
             self.ffn = FFN(d_model, dim_feedforward, dropout)
 
-    # def forward(
-    #         self,
-    #         tgt: Tensor,
-    #         memory: Tensor,
-    #         arm: Optional[AttentionRefinementModule],
-    #         freqs_cis: Tensor,
-    #         tgt_mask: Optional[Tensor] = None,
-    #         memory_mask: Optional[Tensor] = None,
-    #         tgt_key_padding_mask: Optional[Tensor] = None,
-    #         memory_key_padding_mask: Optional[Tensor] = None,
-    # ) -> Tensor:
-    #     r"""Pass the inputs (and mask) through the decoder layer.
-    #
-    #     Args:
-    #         tgt: the sequence to the decoder layer (required).
-    #         memory: the sequence from the last layer of the encoder (required).
-    #         tgt_mask: the mask for the tgt sequence (optional).
-    #         memory_mask: the mask for the memory sequence (optional).
-    #         tgt_key_padding_mask: the mask for the tgt keys per batch (optional).
-    #         memory_key_padding_mask: the mask for the memory keys per batch (optional).
-    #
-    #     Shape:
-    #         see the docs in Transformer class.
-    #     """
-    #     tgt_norm = self.norm1(tgt)  # pre-norm
-    #     tgt2 = self.self_attn(
-    #         tgt_norm, tgt_norm, tgt_norm, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask, freqs_cis=freqs_cis
-    #     )[0]
-    #     tgt = tgt + self.dropout1(tgt2)
-    #
-    #     tgt_norm = self.norm2(tgt)  # pre-norm
-    #     tgt2, attn = self.multihead_attn(
-    #         tgt_norm,
-    #         memory,
-    #         memory,
-    #         arm=arm,
-    #         attn_mask=memory_mask,
-    #         key_padding_mask=memory_key_padding_mask,
-    #     )
-    #     tgt = tgt + self.dropout2(tgt2)
-    #
-    #     tgt_norm = self.norm3(tgt)  # pre-norm
-    #     if self.use_moe:
-    #         tgt2, l_aux = self.ffn(tgt_norm)
-    #     else:
-    #         tgt2 = self.ffn(tgt_norm)
-    #         l_aux = None
-    #
-    #     tgt = tgt + self.dropout3(tgt2)
-    #     return tgt, attn, l_aux
-
     def forward(
             self,
             tgt: Tensor,
@@ -178,14 +122,28 @@ class TransformerDecoderLayer(nn.Module):
             tgt_key_padding_mask: Optional[Tensor] = None,
             memory_key_padding_mask: Optional[Tensor] = None,
     ) -> Tensor:
+        r"""Pass the inputs (and mask) through the decoder layer.
+
+        Args:
+            tgt: the sequence to the decoder layer (required).
+            memory: the sequence from the last layer of the encoder (required).
+            tgt_mask: the mask for the tgt sequence (optional).
+            memory_mask: the mask for the memory sequence (optional).
+            tgt_key_padding_mask: the mask for the tgt keys per batch (optional).
+            memory_key_padding_mask: the mask for the memory keys per batch (optional).
+
+        Shape:
+            see the docs in Transformer class.
+        """
+        tgt_norm = self.norm1(tgt)  # pre-norm
         tgt2 = self.self_attn(
-            tgt, tgt, tgt, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask,
-            freqs_cis=freqs_cis
+            tgt_norm, tgt_norm, tgt_norm, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask, freqs_cis=freqs_cis
         )[0]
         tgt = tgt + self.dropout1(tgt2)
-        tgt = self.norm1(tgt)  # post-norm
+
+        tgt_norm = self.norm2(tgt)  # pre-norm
         tgt2, attn = self.multihead_attn(
-            tgt,
+            tgt_norm,
             memory,
             memory,
             arm=arm,
@@ -193,15 +151,52 @@ class TransformerDecoderLayer(nn.Module):
             key_padding_mask=memory_key_padding_mask,
         )
         tgt = tgt + self.dropout2(tgt2)
-        tgt = self.norm2(tgt)  # post-norm
+
+        tgt_norm = self.norm3(tgt)  # pre-norm
         if self.use_moe:
-            tgt2, l_aux = self.ffn(tgt)
+            tgt2, l_aux = self.ffn(tgt_norm)
         else:
-            tgt2 = self.ffn(tgt)
+            tgt2 = self.ffn(tgt_norm)
             l_aux = None
+
         tgt = tgt + self.dropout3(tgt2)
-        tgt = self.norm3(tgt)  # post-norm
         return tgt, attn, l_aux
+
+    # def forward(
+    #         self,
+    #         tgt: Tensor,
+    #         memory: Tensor,
+    #         arm: Optional[AttentionRefinementModule],
+    #         freqs_cis: Tensor,
+    #         tgt_mask: Optional[Tensor] = None,
+    #         memory_mask: Optional[Tensor] = None,
+    #         tgt_key_padding_mask: Optional[Tensor] = None,
+    #         memory_key_padding_mask: Optional[Tensor] = None,
+    # ) -> Tensor:
+    #     tgt2 = self.self_attn(
+    #         tgt, tgt, tgt, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask,
+    #         freqs_cis=freqs_cis
+    #     )[0]
+    #     tgt = tgt + self.dropout1(tgt2)
+    #     tgt = self.norm1(tgt)  # post-norm
+    #     tgt2, attn = self.multihead_attn(
+    #         tgt,
+    #         memory,
+    #         memory,
+    #         arm=arm,
+    #         attn_mask=memory_mask,
+    #         key_padding_mask=memory_key_padding_mask,
+    #     )
+    #     tgt = tgt + self.dropout2(tgt2)
+    #     tgt = self.norm2(tgt)  # post-norm
+    #     if self.use_moe:
+    #         tgt2, l_aux = self.ffn(tgt)
+    #     else:
+    #         tgt2 = self.ffn(tgt)
+    #         l_aux = None
+    #     tgt = tgt + self.dropout3(tgt2)
+    #     tgt = self.norm3(tgt)  # post-norm
+    #     return tgt, attn, l_aux
 
 
 class TransformerDecoder(nn.Module):
@@ -236,8 +231,8 @@ class TransformerDecoder(nn.Module):
             )
             for _ in range(num_layers)
         ])
-        # self.norm = nn.LayerNorm(d_model)
-        self.norm = None
+        self.norm = nn.LayerNorm(d_model)
+        # self.norm = None
         self.arm = arm
 
         self.register_buffer(
@@ -256,8 +251,8 @@ class TransformerDecoder(nn.Module):
             memory_key_padding_mask: Optional[Tensor] = None,
     ) -> Tensor:
         output = tgt
-        # current_freqs_cis = self.freqs_cis[:tgt.size(0)]
-        current_freqs_cis = None
+        current_freqs_cis = self.freqs_cis[:tgt.size(0)]
+        # current_freqs_cis = None
         arm = None
         l_aux = 0.0 if self.use_moe else None
         for i, mod in enumerate(self.layers):
@@ -277,8 +272,8 @@ class TransformerDecoder(nn.Module):
             if self.use_moe and cur_l_aux is not None:
                 l_aux += cur_l_aux
 
-        if self.norm is not None:
-            output = self.norm(output)
+        # if self.norm is not None:
+        output = self.norm(output)
 
         return output, l_aux
 
@@ -305,9 +300,6 @@ class Decoder(DecodeModel):
 
         self.word_embed = nn.Embedding(vocab_size, d_model)
         self.pos_enc = WordPosEnc(d_model=d_model)
-        # self.norm = nn.LayerNorm(d_model)
-
-        # self.dropout = nn.Dropout(dropout)
 
         self.model = TransformerDecoder(
             d_model=d_model,
@@ -362,8 +354,6 @@ class Decoder(DecodeModel):
 
         tgt = self.word_embed(tgt)
         tgt = self.pos_enc(tgt)
-        # tgt = self.norm(tgt)
-        # tgt = self.dropout(tgt)
 
         h = src.shape[1]
         src = rearrange(src, "b h w d -> (h w) b d")
